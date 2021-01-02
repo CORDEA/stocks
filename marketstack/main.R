d1dac0f6777e8eb4327fe717a61e635d54983ccf
Yoshihiro Tanaka <contact@cordea.jp>
2021-01-02 13:20:53 +0900

    fetch data

---
+++ marketstack/main.R
@@ -0,0 +1,24 @@
+library(jsonlite)
+
+base_url <- "https://api.marketstack.com"
+url <- paste(base_url, "/v1/eod/latest", sep = "")
+
+query = list(
+  token = "",
+  symbols = "",
+  from = "",
+  to = ""
+)
+
+join <- function(a, b) {
+  q <- paste(b, query[[b]], sep = "=")
+  if (grepl("?", a, fixed = TRUE)) {
+    paste(a, q, sep = "&")
+  } else {
+    paste(a, q, sep = "?")
+  }
+}
+url <- Reduce(join, names(query), url)
+
+r <- fromJSON(url)
+data <- r$data
